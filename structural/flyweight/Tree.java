9da482df420a647f67589e92a7146268d6993451
Cody-Benson <codyobenson@gmail.com>
2025-09-02 14:24:23 -0500

    working on flyweight pattern

---
+++ structural/flyweight/Tree.java
@@ -0,0 +1,9 @@
+public class Tree {
+    public int position;
+    public String color;
+
+    public Tree(int position, String color){
+        this.position = position;
+        this.color = color;
+    }
+}
